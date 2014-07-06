63ede7cb6388e4a0fc527a7a03feaabe330bccdf
Florian Jenett <fjenett@gmail.com>
2014-07-06 23:05:24 +0200

    slide to anchor for frontpage

--- sites/all/modules/custom/scrolling_panels_page/plugins/styles/sliding_region/sliding-region-pane.tpl.php
+++ sites/all/modules/custom/scrolling_panels_page/plugins/styles/sliding_region/sliding-region-pane.tpl.php
@@ -3,7 +3,9 @@
 	if ( !empty( $settings['background_image_file'] ) )
 		$bg_image_path = $settings['background_image_file']['style_paths']['large']['path'];
 
-?><div class="pane" style="background-image:url(<?php print $bg_image_path ?>)">
+?><div class="pane" style="background-image:url(<?php print $bg_image_path ?>) <?php 
+		if ( property_exists($content,'css_class') ) print $content->css_class 
+	?>" id="<?php if ( property_exists($content,'css_id') ) print $content->css_id ?>">
 	<div class="inner">
 		<?php if (isset($content->title)): ?>
 			<h2 class="pane-title"><?php print $content->title; ?></h2>
--- sites/all/themes/ccl/js/scrolling-front-page.js
+++ sites/all/themes/ccl/js/scrolling-front-page.js
@@ -1,3 +1,31 @@
 jQuery(function(){
-	//console.log( 'Loaded' );
+
+	// add scroll-to anchor effect to menu and links with scroll-to-link class
+    // http://css-tricks.com/snippets/jquery/smooth-scrolling/
+    jQuery('a').click(function() {
+        if ( location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && 
+             location.hostname == this.hostname ) 
+        {
+            var $target = jQuery(this.hash);
+            $target = $target.length ? $target : jQuery('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                
+                var t = $target.offset().top;
+
+                jQuery('a').removeClass('active');
+                jQuery(this).addClass('active');
+
+                jQuery('html,body').animate({
+                    scrollTop: t
+                }, {
+                    done : function () {
+                        
+                    },
+                    duration: 650
+                });
+                return false;
+            }
+        }
+    });
+
 });
