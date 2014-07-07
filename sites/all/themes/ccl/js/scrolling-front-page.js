6cd3ef059f12d994bbf750a36bb87bdacfaccd99
Florian Jenett <fjenett@gmail.com>
2014-07-07 23:19:44 +0200

    some style for projects, participants

--- sites/all/themes/ccl/js/scrolling-front-page.js
+++ sites/all/themes/ccl/js/scrolling-front-page.js
@@ -1,5 +1,7 @@
 jQuery(function(){
 
+    var header_offset_top = 80;
+
 	// add scroll-to anchor effect to menu and links with scroll-to-link class
     // http://css-tricks.com/snippets/jquery/smooth-scrolling/
     jQuery('a').click(function() {
@@ -8,9 +10,14 @@ jQuery(function(){
         {
             var $target = jQuery(this.hash);
             $target = $target.length ? $target : jQuery('[name=' + this.hash.slice(1) + ']');
-            if ($target.length) {
-                
-                var t = $target.offset().top;
+            if ( $target.length == 1 ) {
+
+                var ttop = $target.data('top');
+                if ( !ttop ) {
+                    ttop = $target.offset().top;
+                    $target.data('top',ttop);
+                }
+                var t = ttop - header_offset_top;
 
                 jQuery('a').removeClass('active');
                 jQuery(this).addClass('active');
@@ -28,5 +35,16 @@ jQuery(function(){
     });
 
 
+    jQuery('.row-class .sticky-in-region').stick_in_parent({
+        parent: '.row-class',
+        offset_top: header_offset_top
+    });
+
+    jQuery( '#header' ).stick_in_parent({
+        parent: '#page',
+        offset_top: -35
+    }).on('sticky_kit:stick',function(){
+        
+    });
 
 });
