86f7ee36d2c235746a01a4ef9618450e2140bd71
Florian Jenett <fjenett@gmail.com>
2014-07-08 22:19:40 +0200

    projects, participants style and theme for frontpage

--- sites/all/themes/ccl/templates/views-view-fields--participants--page.tpl.php
+++ sites/all/themes/ccl/templates/views-view-fields--participants--page.tpl.php
@@ -2,7 +2,6 @@
 <div class="participant" style="background-image: url(<?php print $fields['picture']->content ?>)">
 	<div class="content">
 		<div class="name"><a href="<?php print $fields['field_website']->content ?>" target="_blank"><?php print $fields['name']->content ?></a></div>
-		<div class="labs"><?php print $fields['title']->content ?></div>
-		<div class="projects"><?php print $fields['view']->content ?></div>
+		<div class="labs"><?php if ( !empty($fields['view']) ) { print $fields['view']->content; } ?></div>
 	</div>
 </div>
