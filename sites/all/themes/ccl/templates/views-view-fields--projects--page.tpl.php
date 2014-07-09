3f5a1f32f1870225b4b5ce132d23d91aa65d3c07
Florian Jenett <fjenett@gmail.com>
2014-07-09 10:59:26 +0200

    added project links to frontpage

--- sites/all/themes/ccl/templates/views-view-fields--projects--page.tpl.php
+++ sites/all/themes/ccl/templates/views-view-fields--projects--page.tpl.php
@@ -1,7 +1,16 @@
 <div class="ratio"></div>
 <div class="project" style="background-image: url(<?php print $fields['field_images_2']->content ?>)">
 	<div class="content">
-		<div class="title"><?php print $fields['title']->content ?></div>
+		<div class="title"><?php 
+			$url = '';
+			if ( !empty( $fields['field_url']->content ) ) {
+				$url = $fields['field_url']->content;
+			} else if ( !empty( $fields['field_source_url']->content ) ) {
+				$url = $fields['field_source_url']->content;
+			}
+			if ( !empty( $url ) ) print l( $fields['title']->content, $url, array( 'attributes' => array( 'target' => '_blank' ) ) );
+			else print $fields['title']->content;
+		?></div>
 		<div class="participants"><?php print $fields['view']->content ?></div>
 	</div>
 </div>
